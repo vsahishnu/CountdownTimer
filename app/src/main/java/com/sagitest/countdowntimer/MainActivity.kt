b162c9b290f5947257c5830e584471790de1cde3
vsahishnu <vsahishnu@gmail.com>
2021-05-27 19:55:15 +0530

    added support for manual time enrty.
    updated the rest of code to support this feature

--- app/src/main/java/com/sagitest/countdowntimer/MainActivity.kt
+++ app/src/main/java/com/sagitest/countdowntimer/MainActivity.kt
@@ -6,24 +6,21 @@ import android.os.Bundle
 import android.content.SharedPreferences
 import android.os.PersistableBundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    val SECONDS_MILLIES = 1 * 1000L
-    val START_MILLIS = 10 * 60 * SECONDS_MILLIES
+    var START_MILLIS: Long ?= null
 
     private var cdt: CountDownTimer ?= null
     var Runner = false
-    var TimeLeftInMillis: Long = START_MILLIS
+    var TimeLeftInMillis: Long ?= null
     var TimeEnd: Long = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        updateCountDownText()
-        buttonUpper()
     }
 
     fun TReact(view: View) {
@@ -36,10 +33,33 @@ class MainActivity : AppCompatActivity() {
 
     fun TReset(view: View) {resetTimer()}
 
-    private fun startTimer() {
-        TimeEnd = System.currentTimeMillis() + TimeLeftInMillis
+    fun TSet(view: View) {
+        var input: String = et_input.text.toString()
+        if (input.length == 0) {
+            Toast.makeText(this@MainActivity, "Field can't be empty", Toast.LENGTH_SHORT).show()
+            return
+        }
 
-        cdt = object : CountDownTimer( TimeLeftInMillis, SECONDS_MILLIES) {
+        var millisInput: Long = input.toLong() * 60000
+        var zero: Long = 0
+        if (millisInput == zero) {
+            Toast.makeText(this@MainActivity, "Please enter a positive number", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        setTimer(millisInput)
+        et_input.setText("")
+    }
+
+    private fun setTimer(milliseconds: Long) {
+        START_MILLIS = milliseconds
+        resetTimer()
+    }
+
+    private fun startTimer() {
+        TimeEnd = System.currentTimeMillis() + TimeLeftInMillis!!
+
+        cdt = object : CountDownTimer( TimeLeftInMillis!!, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 TimeLeftInMillis = millisUntilFinished
                 updateCountDownText()
@@ -47,36 +67,49 @@ class MainActivity : AppCompatActivity() {
             override fun onFinish() {
                 Runner = false
                 btnSP.text = "Start"
-                buttonUpper()
+                watchInterfaceUpper()
             }
         }.start()
 
         Runner = true
-        buttonUpper()
+        watchInterfaceUpper()
     }
 
     private fun pauseTimer() {
         cdt?.cancel()
         Runner = false
-        buttonUpper()
+        watchInterfaceUpper()
     }
 
     private fun resetTimer() {
         TimeLeftInMillis = START_MILLIS
         updateCountDownText()
-        buttonUpper()
+        watchInterfaceUpper()
     }
 
     private fun updateCountDownText() {
-        var secs = (TimeLeftInMillis/1000).toInt()
-        val mins = secs / 60
-        secs %= 60
+        var hrs: Int = ((TimeLeftInMillis!! / 1000) / 3600).toInt()
+        var mins: Int = (((TimeLeftInMillis!! / 1000) % 3600) / 60).toInt()
+        var secs: Int = ((TimeLeftInMillis!! / 1000) % 60).toInt()
 
-        tvCounter.text = String.format("%02d:%02d", mins, secs)
+        if (hrs > 0) {
+            tvCounter.text = String.format("%d:%02d:%02d", hrs, mins, secs)
+        } else {
+            tvCounter.text = String.format("%02d:%02d", mins, secs)
+        }
+
+
+        //var secs = (TimeLeftInMillis!!/1000).toInt()
+        //val mins = secs / 60
+        //val hrs = mins / 60
+        //secs %= 60
+        //tvCounter.text = String.format("%02d:%02d", mins, secs)
     }
 
-    private fun buttonUpper() {
+    private fun watchInterfaceUpper() {
         if (Runner) {
+            et_input.visibility = View.INVISIBLE
+            btn_set.visibility = View.INVISIBLE
             btnReset.visibility = View.INVISIBLE
             btnSP.text = "Pause"
         } else {
@@ -84,17 +117,19 @@ class MainActivity : AppCompatActivity() {
 //                -can be at 0 so reset visible and start not visible
 //                -at full time so start visible and reset not visible
 //                -not full or end so both start and reset are visible
+            et_input.visibility = View.VISIBLE
+            btn_set.visibility = View.VISIBLE
             btnSP.text = "Start"
 
             //for time somewhere in the middle of time
-            if (TimeLeftInMillis < 1000) {
+            if (TimeLeftInMillis!! < 1000) {
                 btnSP.visibility = View.INVISIBLE
             } else {
                 btnSP.visibility = View.VISIBLE
             }
 
             //not full
-            if (TimeLeftInMillis < START_MILLIS) {
+            if (TimeLeftInMillis!! < START_MILLIS!!) {
                 btnReset.visibility = View.VISIBLE
             } else {
                 btnReset.visibility = View.INVISIBLE
@@ -102,38 +137,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    //onSave and onRestore are for orientation only
-    //onStop and onStart is for background support and orientation
-    /*
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putLong("MilliTimeLeft", TimeLeftInMillis)
-        outState.putBoolean("Workin", Runner)
-        outState.putLong("Ender", TimeEnd)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-
-        TimeLeftInMillis = savedInstanceState.getLong("MilliTimeLeft")
-        Runner = savedInstanceState.getBoolean("Workin")
-        updateCountDownText()
-        buttonUpper()
-
-        if (Runner) {
-            TimeEnd = savedInstanceState.getLong("Ender")
-            startTimer()
-        }
-    }
-     */
-
     override fun onStop() {
         super.onStop()
 
         val prefs: SharedPreferences = getSharedPreferences("prefs", MODE_PRIVATE)
         val editor:SharedPreferences.Editor =  prefs.edit()
 
-        editor.putLong("MilliTimeLeft", TimeLeftInMillis)
+        editor.putLong("startMillis", START_MILLIS!!)
+        editor.putLong("MilliTimeLeft", TimeLeftInMillis!!)
         editor.putBoolean("Workin", Runner)
         editor.putLong("Ender", TimeEnd)
 
@@ -149,22 +160,23 @@ class MainActivity : AppCompatActivity() {
 
         var prefs: SharedPreferences = getSharedPreferences("prefs", MODE_PRIVATE)
 
-        TimeLeftInMillis = prefs.getLong("MilliTimeLeft", START_MILLIS)
+        START_MILLIS = prefs.getLong("startMillis", 900000)
+        TimeLeftInMillis = prefs.getLong("MilliTimeLeft", START_MILLIS!!)
         Runner = prefs.getBoolean("Workin", false)
 
         updateCountDownText()
-        buttonUpper()
+        watchInterfaceUpper()
 
         if (Runner) {
             TimeEnd = prefs.getLong("Ender", 0)
             TimeLeftInMillis = TimeEnd - System.currentTimeMillis()
 
-            if (TimeLeftInMillis < 0) {
+            if (TimeLeftInMillis!! < 0) {
                 TimeLeftInMillis = 0
                 Runner = false
 
                 updateCountDownText()
-                buttonUpper()
+                watchInterfaceUpper()
             } else {
                 startTimer()
             }
@@ -172,3 +184,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+//onSave and onRestore are for orientation only
+//onStop and onStart is for background support and orientation
+/*
+override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    outState.putLong("MilliTimeLeft", TimeLeftInMillis)
+    outState.putBoolean("Workin", Runner)
+    outState.putLong("Ender", TimeEnd)
+}
+
+override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+    super.onRestoreInstanceState(savedInstanceState)
+
+    TimeLeftInMillis = savedInstanceState.getLong("MilliTimeLeft")
+    Runner = savedInstanceState.getBoolean("Workin")
+    updateCountDownText()
+    buttonUpper()
+
+    if (Runner) {
+        TimeEnd = savedInstanceState.getLong("Ender")
+        startTimer()
+    }
+}
+ */
